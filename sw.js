79189b25be839adf8ebbd9bf1a9779e16f4361a2
samadhikudamage <nipun@yaalalabs.com>
2023-03-03 18:52:12 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b8c217466c0bf3778102.js"
+    "url": "webpack-runtime-f8b43c0692dfd83a3f78.js"
   },
   {
     "url": "framework-59619f3b9a65445c9952.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cea86861cad0dbcabccbc2f34e2acd45"
+    "revision": "97495ee217242cfc9a004cebf8e5c4c7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
